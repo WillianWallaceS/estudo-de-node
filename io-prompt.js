c0aae993f0999a968685dc6c8c64e9d974ede2fe
Willian_Wallace <125695509+WillianWallaceS@users.noreply.github.com>
2025-01-04 09:50:41 -0300

    IO V2
    Foi colocados comentários para descrever os códigos novos usados

--- io-prompt.js
+++ io-prompt.js
@@ -1,4 +1,7 @@
-const rl = require('readline')
+//IO - entrada e sa´da de dados/input e output 
+
+
+const rl = require('readline') //requerer uma biblioteca
 
 const prompt = rl.createInterface({
     input: process.stdin,
@@ -7,9 +10,9 @@ const prompt = rl.createInterface({
 
 prompt.question('Qual é seu número favorito?: ', (resposta) =>{
     console.log(`O dobro do seu número favorito é: ${parseInt(resposta)* 2}`)
-    prompt.close()
+    prompt.close() //se usa dentro da função pois dará o tempo de resposta 
 })
-const promptPromise = {
+const promptPromise = { //Promise para facilitar a execução
     question: (pergunta) => new Promise((resolve,reject) =>{
         try{
             prompt.question(pergunta), (resposta) => resolve(resposta)
